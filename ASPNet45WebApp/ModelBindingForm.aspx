9a9a2983a6bceb5ecd015b8a8c359f0ca5c4827c
Maheshkumar <maheshkumarn@live.com>
2017-08-21 13:17:14 +0530

    ModelBindings

---
+++ ASPNet45WebApp/ModelBindingForm.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ModelBindingForm.aspx.cs" Inherits="ASPNet45WebApp.ModelBindingForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>ModelBinding Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Country: <asp:DropDownList runat="server" ID="Country"
+                SelectMethod="CountryDropDownList_GetCountry" AutoPostBack="true">
+                <asp:ListItem Text="-Select-" />
+            </asp:DropDownList>
+            <asp:GridView runat="server" ID="CustomerGridView" 
+                ItemType="ASPNet45WebApp.Models.Customer" DataKeyNames="CustomerID"
+                SelectMethod="CustomerGridView_GetData"
+                UpdateMethod="CustomerGridView_UpdateItem"
+
+                AutoGenerateColumns="false"
+                AutoGenerateEditButton ="true"
+                AllowPaging="true" PageSize="10"
+                >
+                <Columns>
+                    <asp:DynamicField DataField="CustomerID" ReadOnly="true" />
+                    <asp:DynamicField DataField="CompanyName" />
+                    <asp:DynamicField DataField="ContactName" />
+                    <asp:DynamicField DataField="City" />
+                    <asp:DynamicField DataField="Country" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASPNet45WebApp/ModelBindingForm.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ASPNet45WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.ModelBinding;
+
+namespace ASPNet45WebApp
+{
+    public partial class ModelBindingForm : System.Web.UI.Page
+    {
+        EfCustomerRepository _customer;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _customer = new EfCustomerRepository();
+        }
+
+        public List<string> CountryDropDownList_GetCountry(object sender, EventArgs e)
+        {
+            var result = new List<string>();
+            result.Add("All");
+            result.AddRange(_customer.Customers.Select(c => c.Country).Distinct().ToList());
+            return result;
+        }
+
+        // The return type can be changed to IEnumerable, however to support
+        // paging and sorting, the following parameters must be added:
+        //     int maximumRows
+        //     int startRowIndex
+        //     out int totalRowCount
+        //     string sortByExpression
+        public IQueryable<ASPNet45WebApp.Models.Customer> CustomerGridView_GetData([Control] string country)
+        {
+            var result = _customer.Customers.ToList();
+            if (country != null)
+            {
+                if (country != "All")
+                {
+                     return result.Where(c => c.Country == country).AsQueryable();
+                }
+            }
+            return result.AsQueryable();
+        }
+
+        // The id parameter name should match the DataKeyNames value set on the control
+        public void CustomerGridView_UpdateItem(string CustomerID)
+        {
+            ASPNet45WebApp.Models.Customer item = _customer.Customers.FirstOrDefault(c=>c.CustomerID == CustomerID);
+            if (item == null)
+            {
+                ModelState.AddModelError("", String.Format("Item with id {0} was not found", CustomerID));
+                return;
+            }
+            TryUpdateModel(item);
+            if (ModelState.IsValid)
+            {
+                _customer.SaveChanges();
+            }
+        }
+    }
+}
